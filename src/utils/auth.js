0c4e43c4db349e5b993835fbec9ae790cf463784
Оксанв Быкова <ishkulova.oksana1@yandex.ru>
2023-09-10 13:22:08 +0400

    исправить урл

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,5 @@
- //export const BASE_URL = 'https://api.oksana.bikova.movies.nomoredomains.xyz';
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://api.oksana.bikova.movies.nomoredomains.xyz';
+//export const BASE_URL = 'http://localhost:3000';
 
 
 function checkResponse(res) {
