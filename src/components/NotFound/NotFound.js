4de3bdd63666ee9af0478f66632f7d1b090edd11
Оксанв Быкова <ishkulova.oksana1@yandex.ru>
2023-09-10 13:21:07 +0400

    исправить поведение кнопки назад на странице 404

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,16 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./NotFound.css";
 
 function NotFound() {
+  const nav = useNavigate();
   return (
     <main>
     <section className="not-found">
       <h1 className="not-found__title">404</h1>
       <p className="not-found__information">Страница не найдена</p>
-      <Link to="" className="not-found__link">
-        Назад
-      </Link>
+      <Link className="not-found__link" onClick={()=> nav(-1)}>Назад</Link>
     </section>
     </main>
   );
